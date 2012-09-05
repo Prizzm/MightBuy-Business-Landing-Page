1efc9d3411b9fa0ca9af771b30dce02d3470750d
Sam Baumgarten <sbaumgarten@makrr.com>
2012-09-04 21:19:44 -0700

    Update Mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 MbBusiness::Application.initialize!
+
+config.action_mailer.default_url_options = { :host => 'sheltered-forest-2536.herokuapp.com' }
