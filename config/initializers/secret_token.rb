f9b8ec4eabfa108166d6ab12574538fc285669d9
Sam Baumgarten <sbaumgarten@makrr.com>
2012-09-04 20:24:25 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MbBusiness::Application.config.secret_token = 'a6d4535ec5d4ed15571d4d4717fc2f55cb8dc358a32649eb3870e70776ff5c243556c8b828373e44e8f38c9c4a3778b99d18c7c0ad42d3b286ed91fd2048189f'
